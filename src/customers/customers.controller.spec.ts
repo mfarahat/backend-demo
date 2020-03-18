29959cf2f456b237beef102eedea0f3d7e1fc799
Mahmoud Farahat <mahmoudkfarahat@gmail.com>
2020-03-18 18:39:45 -0500

    - added customer findOne impelemntation - added customer findAll impelemntation - added test for findOne - added test for findAll

--- src/customers/customers.controller.spec.ts
+++ src/customers/customers.controller.spec.ts
@@ -1,18 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CustomersController } from './customers.controller';
+import { CustomersService } from './customers.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CustomerEntity } from './customer.entity';
+import { Repository } from 'typeorm';
 
 describe('Customers Controller', () => {
-  let controller: CustomersController;
+  let customerController: CustomersController;
+  let customerService: CustomersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CustomersController],
+      providers: [
+        CustomersService,
+        {
+          provide: getRepositoryToken(CustomerEntity),
+          useClass: Repository,
+        }]
     }).compile();
 
-    controller = module.get<CustomersController>(CustomersController);
+    customerController = module.get<CustomersController>(CustomersController);
+    customerService = module.get<CustomersService>(CustomersService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(customerController).toBeDefined();
+  });
+
+  it('should return all customers', async () => {
+    const mockedCustomers: CustomerEntity[] = [{
+      id: 1,
+      firstName: "Katerine",
+      lastName: "Pyrton",
+    }, {
+      id: 2,
+      firstName: "Lucilia",
+      lastName: "Strasse",
+    }];
+
+    jest.spyOn(customerService, 'findAll').mockResolvedValueOnce(mockedCustomers);
+    expect(await customerController.findAll()).toEqual(mockedCustomers);
+  });
+
+  it('should return one customer', async () => {
+    const mockedCustomer: CustomerEntity = {
+      id: 1,
+      firstName: "Katerine",
+      lastName: "Pyrton",
+    };
+
+    jest.spyOn(customerService, 'findOne').mockResolvedValueOnce(mockedCustomer);
+    expect(await customerController.findOne(1)).toEqual(mockedCustomer);
   });
 });
--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { CustomerEntity } from './customer.entity';
+import { CustomersService } from './customers.service';
 
 @Controller('customers')
-export class CustomersController {}
+export class CustomersController {
+    constructor(private readonly customerService: CustomersService) { }
+
+    @Get()
+    findAll(): Promise<CustomerEntity[]> {
+        return this.customerService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id', new ParseIntPipe()) id: number): Promise<CustomerEntity> {
+        return this.customerService.findOne(id);
+    }
+}
