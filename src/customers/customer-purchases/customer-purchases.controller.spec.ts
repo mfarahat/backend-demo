4c3adab0f081a18a2d2c852964513eb501e7d6a8
Mahmoud Farahat <mahmoudkfarahat@gmail.com>
2020-04-18 10:32:12 -0500

    added test for customer purchases report

--- src/customers/customer-purchases/customer-purchases.controller.spec.ts
+++ src/customers/customer-purchases/customer-purchases.controller.spec.ts
@@ -5,6 +5,8 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { Purchase } from './../../purchases/purchase.entity';
 import { PurchaseChartRepository } from './purchase-chart.repository';
 import { Repository } from 'typeorm';
+import { GroupedPurchasesReportDto } from './dto/purchases-reports';
+import { DateLimitsDto } from './dto/date-limits';
 
 describe('CustomerPurchases Controller', () => {
   let customerPurchaseController: CustomerPurchasesController;
@@ -31,5 +33,29 @@ describe('CustomerPurchases Controller', () => {
 
   it('should be defined', () => {
     expect(customerPurchaseController).toBeDefined();
+  });
+
+  it('should return customer purchase report', async () => {
+    const mockedReport: GroupedPurchasesReportDto = {
+      "purchases": [{
+        "date": new Date(), "points": 0, "value": 36
+      }, {
+        "date": new Date(), "value": 0, "points": 0
+      }],
+      "totalPoints": 100, "totalValue": 100
+    };
+
+    jest.spyOn(customerPurchaseService, 'getPurchaseData').mockResolvedValueOnce(mockedReport);
+    expect(await customerPurchaseController.getPurchaseData(5)).toEqual(mockedReport);
+  });
+
+  it('should return date ranges for customers', async () => {
+    const mockedDateLimits: DateLimitsDto = {
+      min:new Date(),
+      max:new Date(),
+    };
+
+    jest.spyOn(customerPurchaseService, 'getDateLimits').mockResolvedValueOnce(mockedDateLimits);
+    expect(await customerPurchaseController.getDateLimits(5)).toEqual(mockedDateLimits);
   });
 });
